675801782202e57ed79eeca0827e454683905d8c
Arslanoov <rasularslanoov@gmail.com>
2022-02-12 23:21:20 +0500

    build(app): Added html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -7,6 +8,12 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Demo',
+      template: 'demo/index.html'
+    })
+  ],
   module: {
     rules: [
       {
